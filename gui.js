ef6020a5931fa8e2e5506d1e46fa840c9ace3d36
Marian Pekár <marian.pekar@saabczech.cz>
2019-09-15 02:01:11 +0200

    Change v1 to v1t and v2 to v2u

--- gui.js
+++ gui.js
@@ -1,13 +1,13 @@
 let gui = new dat.GUI({ autoPlace: false });
 
-let vector1Folder = gui.addFolder("v1 * t")
+let vector1Folder = gui.addFolder("v1t")
 vector1Folder.add(args, 'v1x', -10, 10).name("X").step(0.01).onChange(draw);
 vector1Folder.add(args, 'v1y', -10, 10).name("Y").step(0.01).onChange(draw);
 vector1Folder.add(args, 'v1z', -10, 10).name("Z").step(0.01).onChange(draw);
 vector1Folder.add(args, 't', -10, 10).name("t").step(0.01).onChange(draw);
 vector1Folder.open();
 
-let vector2Folder = gui.addFolder("v2 * u")
+let vector2Folder = gui.addFolder("v2u")
 vector2Folder.add(args, 'v2x', -10, 10).name("X").step(0.01).onChange(draw);
 vector2Folder.add(args, 'v2y', -10, 10).name("Y").step(0.01).onChange(draw);
 vector2Folder.add(args, 'v2z', -10, 10).name("Z").step(0.01).onChange(draw);
@@ -15,11 +15,11 @@ vector2Folder.add(args, 'u', -10, 10).name("u").step(0.01).onChange(draw);
 vector2Folder.open();
 
 let productsFolder = gui.addFolder("Show products");
-productsFolder.add(args, 'showV1plusV2').name("a = v1 + v2").onChange(draw);
-productsFolder.add(args, 'showV1minusV2').name("b = v1 - v2").onChange(draw);
-productsFolder.add(args, 'showV2minusV1').name("b' = v2 - v1").onChange(draw);
-productsFolder.add(args, 'showV1crossV2').name("c = v1 ✕ v2").onChange(draw);
-productsFolder.add(args, 'showV2crossV1').name("c' = v2 ✕ v1").onChange(draw);
+productsFolder.add(args, 'showV1plusV2').name("a = v1t + v2u").onChange(draw);
+productsFolder.add(args, 'showV1minusV2').name("b = v1t - v2u").onChange(draw);
+productsFolder.add(args, 'showV2minusV1').name("b' = v2u - v1t").onChange(draw);
+productsFolder.add(args, 'showV1crossV2').name("c = v1t ✕ v2u").onChange(draw);
+productsFolder.add(args, 'showV2crossV1').name("c' = v2u ✕ v1t").onChange(draw);
 
 gui.add(args, 'showNormalized').name("Show Normalized").onChange(draw);
 gui.add(args, 'showAxis').name("Show Axis").onChange(draw);
