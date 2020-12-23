b9549d4b737689a64bff557d0650836875f22c32
Marian Pekár <marian.pekar@gmail.com>
2020-12-23 21:00:31 +0100

    Change ranges and step sizes

--- gui.js
+++ gui.js
@@ -1,17 +1,17 @@
 let gui = new dat.GUI({ autoPlace: false });
 
 let vector1Folder = gui.addFolder("v1t")
-vector1Folder.add(args, 'v1x', -100, 100).name("X").step(0.001).onChange(draw);
-vector1Folder.add(args, 'v1y', -100, 100).name("Y").step(0.001).onChange(draw);
-vector1Folder.add(args, 'v1z', -100, 100).name("Z").step(0.001).onChange(draw);
-vector1Folder.add(args, 't', -100, 100).name("t").step(0.001).onChange(draw);
+vector1Folder.add(args, 'v1x', -10, 10).name("X").step(0.01).onChange(draw);
+vector1Folder.add(args, 'v1y', -10, 10).name("Y").step(0.01).onChange(draw);
+vector1Folder.add(args, 'v1z', -10, 10).name("Z").step(0.01).onChange(draw);
+vector1Folder.add(args, 't', -10, 10).name("t").step(0.01).onChange(draw);
 vector1Folder.open();
 
 let vector2Folder = gui.addFolder("v2u")
-vector2Folder.add(args, 'v2x', -100, 100).name("X").step(0.001).onChange(draw);
-vector2Folder.add(args, 'v2y', -100, 100).name("Y").step(0.001).onChange(draw);
-vector2Folder.add(args, 'v2z', -100, 100).name("Z").step(0.001).onChange(draw);
-vector2Folder.add(args, 'u', -100, 100).name("u").step(0.001).onChange(draw);
+vector2Folder.add(args, 'v2x', -10, 10).name("X").step(0.01).onChange(draw);
+vector2Folder.add(args, 'v2y', -10, 10).name("Y").step(0.01).onChange(draw);
+vector2Folder.add(args, 'v2z', -10, 10).name("Z").step(0.01).onChange(draw);
+vector2Folder.add(args, 'u', -10, 10).name("u").step(0.01).onChange(draw);
 vector2Folder.open();
 
 let productsFolder = gui.addFolder("Show products");
@@ -26,7 +26,7 @@ gui.add(args, 'showAxis').name("Show Axis").onChange(draw);
 gui.add(args, 'showGrid').name("Show Grid").onChange(draw);
 gui.add(args, 'showResults').name("Show Results").onChange(draw);
 gui.add(args, 'anglesInRad').name("Angles In Rad").onChange(draw);
-gui.add(args, 'decimals', 1, 5).name("Decimals").step(1).onChange(draw);
+gui.add(args, 'decimals', 0, 3).name("Decimals").step(1).onChange(draw);
 
 let guiContainerDOM = document.getElementById('dat-gui-container');
 guiContainerDOM.appendChild(gui.domElement);
